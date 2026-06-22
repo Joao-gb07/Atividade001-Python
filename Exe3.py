8b5fcac7b468152b8ed9902e842c2ddf4e2f8cd9
João <va60234@gmail.com>
2026-06-22 20:00:20 -0300

    Fix Questão 03

--- Exe3.py
+++ Exe3.py
@@ -3,3 +3,10 @@ tamanho_arquivo = float(input("Digite o tamanho do arquivo MB: "))
 velocidadeMbps = float(input("Digite a velocidade do link de Internet: "))
 
 tempo_aproximado = (tamanho_arquivo / velocidadeMbps)
+
+tempo_segundos = (tamanho_arquivo * 8) / velocidadeMbps
+tempo_minutos = tempo_segundos / 60
+
+print(f"Tempo aproximado de download: {tempo_minutos:.2f} minutos")
+
+
