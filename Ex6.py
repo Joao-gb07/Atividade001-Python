0d42013d73b6b8bb7471a1984d5c0e5de79953c1
João Victor <va60234@gmail.com>
2026-06-29 08:24:13 -0300

    Implementando Funções no Codigo

--- Ex6.py
+++ Ex6.py
@@ -1,31 +1,33 @@
 reaisDigitado = float(input("Digite o valor que deseja converter R$ "))
 
-reais_dolar = reaisDigitado / 5.18
-reais_euro = reaisDigitado / 5.88
-reais_libra = reaisDigitado / 6.82
-reais_lene = reaisDigitado * 0.0318
-
+def mostrar_menu():
+        print("================")
+        print("1 - Dólar")
+        print("2 - Euro")
+        print("3 - Libra")
+        print("4 - Lene")
 while True:
-    print("================")
-    print("1 - Dólar")
-    print("2 - Euro")
-    print("3 - Libra")
-    print("4 - Lene")
-    opcao = int(input("Digite uma opção: "))
-    
-    match opcao:
-        case 1:
-            print(f"{reaisDigitado} reais = {reais_dolar:.2f} Dólar")
-            break
-        case 2:
-            print(f"{reaisDigitado} reais = {reais_euro:.2f} Euro")
-            break
-        case 3:
-            print(f"{reaisDigitado} reais = {reais_libra:.2f} Libras")
-            break
-        case 4:
-            print(f"{reaisDigitado} reais = {reais_lene:.2f} Lenes")
-            break
-        case _:
-            print("Opção digitada inválida!")
+        mostrar_menu()
+        opcao = int(input("Digite uma opção: "))
+        
+
+        match opcao:
+            case 1:
+                reais_dolar = reaisDigitado / 5.18
+                print(f"{reaisDigitado} reais = {reais_dolar:.2f} Dólar")
+                break
+            case 2:
+                reais_euro = reaisDigitado / 5.88
+                print(f"{reaisDigitado} reais = {reais_euro:.2f} Euro")
+                break
+            case 3:
+                reais_libra = reaisDigitado / 6.82
+                print(f"{reaisDigitado} reais = {reais_libra:.2f} Libras")
+                break
+            case 4:
+                reais_lene = reaisDigitado * 0.0318
+                print(f"{reaisDigitado} reais = {reais_lene:.2f} Lenes")
+                break
+            case _:
+                print("Opção digitada inválida!")
             
